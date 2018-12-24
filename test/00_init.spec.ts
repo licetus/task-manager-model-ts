dffd034adeb347aa8a7db806de23abbd5b0a2b32
licetus <licetus0316@gmail.com>
2018-12-24 16:02:45 +0800

    install chai-as-promised, add init spec.ts

---
+++ test/00_init.spec.ts
@@ -0,0 +1,23 @@
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import log4js from 'log4js'
+import { PgManager } from '../src/db/manager'
+import config from './config/config.json'
+
+chai.use(chaiAsPromised)
+chai.should()
+
+const log = log4js.getLogger()
+
+before (async() => {
+  const pgConfig = {
+    connections: config.database.postgres
+  }
+  const pgManager = new PgManager(pgConfig)
+  await pgManager.rebuild()
+  log.info(`Current db version: ${pgManager.version}`)
+})
+
+after ((done: Function) => {
+  done()
+})
