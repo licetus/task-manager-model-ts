6eb63e4cf77a6f257e9bfd29e87411e346b2c16a
licetus <licetus0316@gmail.com>
2018-12-26 11:56:59 +0800

    move db classes.ts into index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import pgPromise from 'pg-promise'
 import { findIndex } from 'lodash'
-import { PgClientConfig, PgQueryConfig } from './classes'
+import { ClientConfig, QueryConfig } from 'pg'
 import config from '../../test/config/config.json'
 
 const pgp = pgPromise({
@@ -16,6 +16,53 @@ const pgp = pgPromise({
   //   console.log('----- Query: ', query)
   // },
 })
+
+class PgClientConfig implements ClientConfig {
+  readonly host?: string
+  readonly port?: number
+  readonly database?: string
+  readonly user?: string
+  readonly password?: string
+  readonly default?: boolean = false
+
+  constructor(config: PgClientConfig) {
+    this.host = config.host
+    this.port = config.port
+    this.database = config.database
+    this.user = config.user
+    this.password = config.password
+    this.default = config.default
+  }
+}
+
+class PgQueryConfig implements QueryConfig {
+  readonly name?: string
+  readonly text: string = ''
+  readonly values?: any[]
+  constructor(...args: any[]) {
+    switch (args.length) {
+      case 3:
+        this.name = args[0]
+        this.text = args[1]
+        this.values = args[2]
+        break
+      case 2:
+        if (typeof args[1] === 'string') {
+          this.name = args[0]
+          this.text = args[1]
+        } else {
+          this.text = args[0]
+          this.values = args[1]
+        }
+        break
+      case 1:
+        this.text = args[0]
+        break
+      default:
+        break
+    }
+  }
+}
 
 export interface DatabaseConfig {
   database: { postgres: PgClientConfig[] }
