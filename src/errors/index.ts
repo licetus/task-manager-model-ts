50fd85953f05d3e55210581599ebb6820d74e190
licetus <licetus0316@gmail.com>
2018-12-28 00:47:08 +0800

    add errors, add error test case

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,4 +1,9 @@
-import { makeConstructor } from '@restify-ts/errors'
+const e: any = require('restify-errors')
+
+enum locales {
+  'zh-cn',
+  'en-US',
+}
 
 const normalize = (name: string) => {
   if (!name.endsWith('Error')) {
@@ -7,40 +12,34 @@ const normalize = (name: string) => {
   return name
 }
 
-class ErrorManager {
-  private locales: any
-  private message: string
-
-  constructor(error: { name: string, message?: string }) {
-    const lang = require('../config/config.json').locales
-    this.locales = require(`./locale.${lang}.json`)
-    if (error.message) {
-      this.message = error.message
-    } else {
-      const name = error.name.slice(0, -5)
-      this.message = this.locales[name]
-    }
-  }
-
-  public register = (errors: any ) => {
-    Object.keys(errors).forEach((key) => {
-      const config = errors[key]
-      const errorName = normalize(key)
-      let err
-      switch (typeof config) {
-        case 'number':
-          err = makeConstructor(errorName, {
-            statusCode: config,
-          })
-          break
-        case 'object':
-          err = makeConstructor(errorName, config)
-          break
-        default:
-          throw new Error(`Invalid error config for ${errorName}`)
-      }
-      Object.assign({
-      })
-    })
+e.lang = (error: { name: string, message?: string }) => {
+  const locale = process.env.NODE_LOCALES || 'en-US'
+  const locales = require(`./locale.${locale}.json`)
+  if (error.message) {
+    return error.message
+  } else {
+    const name = error.name.slice(0, -5)
+    return locales[name]
   }
 }
+
+e.register = (errors: any) => {
+  Object.keys(errors).forEach((key) => {
+    const config = errors[key]
+    const errorName = normalize(key)
+    switch (typeof config) {
+      case 'number':
+        e[errorName] = e.makeConstructor(errorName, {
+          statusCode: config,
+        })
+        break
+      case 'object':
+        e[errorName] = e.makeConstructor(errorName, config)
+        break
+      default:
+        throw new Error(`Invalid error config for ${errorName}`)
+    }
+  })
+}
+
+export default e
---
+++ test/02_error.spec.ts
@@ -0,0 +1,28 @@
+import error from '../src/errors'
+
+
+describe('* Error =======================', () => {
+  const ERRORS = {
+    Test: 400,
+  }
+  it('create error', () => {
+    error.register(ERRORS)
+    error.should.have.property('TestError')
+  })
+  it('throw error', () => {
+    // process.env.NODE_LOCALES = 'zh-cn'
+    const m = error.lang({
+      name: 'TestError'
+    })
+    const e = new error.TestError(m)
+    e.name.should.equal('TestError')
+    switch (process.env.NODE_LOCALES) {
+      case 'zh-cn':
+        e.message.should.equal('这是一个测试用异常')
+        break
+      case 'en-US':
+        e.message.should.equal('This is a test error.')
+    }
+    throw e
+  })
+})
